fd0771f72a35d639801613cb8d4a7b97f9b11a4c
hsrsbd <hsraffety@gmail.com>
2019-07-08 10:16:11 -0700

    grabbing information about db from hubs model.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const express = require('express');
+const express = require('express'); // Common JS Modules. Not ES2015 obv.
+
+const db = require('./data/hubs-model');
 
 const server = express();
 
@@ -6,6 +8,16 @@ const port = 5000;
 
 server.get('/', (req, res) => {
   res.send('Hello Node / route');
+});
+
+server.get('/hubs', (req, res) => {
+  db.find()
+    .then(hubs => {
+      res.status(200).json(hubs);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
 server.listen(port, () => {
